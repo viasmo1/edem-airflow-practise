e30e22d97ad438a29d698e527966fc7149965940
Vicent Asensio <vicent.asenmol@gmail.com>
2022-06-10 23:26:34 +0200

    Erase solution of first DAG

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 
 from datetime import datetime
 
@@ -8,13 +7,10 @@ with DAG(
     dag_id="first_sample_dag", start_date=datetime(2022, 5, 28), schedule_interval=None
 ) as dag:
 
-    start_task = DummyOperator(task_id="start")
+    start_task = EmptyOperator(task_id="start")
 
-    print_hello_world = BashOperator(
-        task_id="print_hello_world", bash_command='echo "\n\n\n\nHello MDA!!!\n\n\n"'
-    )
+    # Here the corresponding operator
 
-    end_task = DummyOperator(task_id="end")
+    end_task = EmptyOperator(task_id="end")
 
-start_task >> print_hello_world
-print_hello_world >> end_task
+# Here the DAG dependencies

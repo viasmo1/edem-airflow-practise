ea41a48c7cb5df21a760e0e6795a73fc3d74561b
Vicent Asensio <vicent.asenmol@gmail.com>
2022-06-10 17:45:13 +0200

    Use dummy operatores

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
 
 from datetime import datetime
@@ -8,13 +8,13 @@ with DAG(
     dag_id="first_sample_dag", start_date=datetime(2022, 5, 28), schedule_interval=None
 ) as dag:
 
-    start_task = EmptyOperator(task_id="start")
+    start_task = DummyOperator(task_id="start")
 
     print_hello_world = BashOperator(
         task_id="print_hello_world", bash_command='echo "\n\n\n\nHello MDA!!!\n\n\n"'
     )
 
-    end_task = EmptyOperator(task_id="end")
+    end_task = DummyOperator(task_id="end")
 
 start_task >> print_hello_world
 print_hello_world >> end_task

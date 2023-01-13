7271cd32167017709f77eb1c88cd6d9d4bda1d63
viasmo1 <vicent.asensio@feverup.com>
2023-01-13 10:35:23 +0100

    Modify params of sample_dag

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -3,16 +3,16 @@ from airflow import DAG
 from datetime import datetime
 
 with DAG(
-    dag_id="first_sample_dag",
+    dag_id="sample_dag",
     start_date=datetime(2022, 5, 28),
     schedule_interval=None,
     tags=["sample_dag"],
-    default_args={'retries': 2},
+    default_args={'retries': 1},
 ) as dag:
 
     # Here the corresponding tasks
 
 
 
-# Here the DAG dependencies
+    # Here the DAG dependencies
 

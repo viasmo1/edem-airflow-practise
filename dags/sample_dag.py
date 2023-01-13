8b9c33c0940d9640aa0f4a5eb7983ce7e144d31d
viasmo1 <vicent.asensio@feverup.com>
2023-01-13 09:10:53 +0100

    Mask sample_dag solution

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,6 +1,4 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.bash import BashOperator
 
 from datetime import datetime
 
@@ -12,15 +10,9 @@ with DAG(
     default_args={'retries': 2},
 ) as dag:
 
-    start_task = EmptyOperator(task_id="start")
+    # Here the corresponding tasks
 
-    # Here the corresponding operator
-    log_print = BashOperator(
-        task_id='log_print',
-        bash_command='echo "\n\n\nHELLO MDA!!!\n\n\n"',
-    )
 
-    end_task = EmptyOperator(task_id="end")
 
 # Here the DAG dependencies
-start_task >> log_print >> end_task
+
